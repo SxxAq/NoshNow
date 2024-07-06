c95f232b65bbaec5cffe04a7201f833c4bad274a
SxxAq <aqueel192@gmail.com>
2024-07-06 21:52:01 +0530

    NAvbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className='app'>
+      <Navbar />
     </div>
   )
 }
