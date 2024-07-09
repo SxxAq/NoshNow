8e7e8c20787d95c8ce0f624bcd2047d757cc5941
SxxAq <aqueel192@gmail.com>
2024-07-10 04:22:21 +0530

    explore-menu

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,40 @@
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+const ExploreMenu = ({ category, setCategory }) => {
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Handpicked Just for You</h1>
+      <p className="explore-menu-text">
+        Each dish on our menu is carefully crafted to offer you the best
+        culinary experience. Our chefs use the finest ingredients to create
+        meals that are not only delicious but also nourishing. Explore our
+        curated selections and enjoy a memorable dining experience.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default ExploreMenu;
