00a0dd0f4c749971b69ceebea2f3be8426ba9b7f
SxxAq <aqueel192@gmail.com>
2024-07-12 22:31:00 +0530

    Added food list and remove food

--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -20,4 +20,28 @@ const addFood = async (req, res) => {
     res.json({ success: false, message: "Error" });
   }
 };
-export { addFood };
+// all food list
+const listFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find({});
+    res.json({ success: true, data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+//=========Remove Food Item================
+const removeFood = async (req, res) => {
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`, () => {});
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "Food removed." });
+  } catch (error) {
+    console.log(error);
+    res.send({ success: false, message: "Error" });
+  }
+};
+
+export { addFood, listFood, removeFood };
