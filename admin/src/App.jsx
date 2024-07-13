54c79405e05a9030cdb4de97b60b9ee7f543cd45
SxxAq <aqueel192@gmail.com>
2024-07-14 02:49:04 +0530

    admin panel sidebar nav

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
