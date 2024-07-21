49fc46a8db53977d2dfc5475770f25ce4d7e35a0
SxxAq <aqueel192@gmail.com>
2024-07-21 20:27:27 +0530

    fetch cart backend

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -37,6 +37,15 @@ const removeFromCart = async (req, res) => {
 
 // fetch cart
 
-const getCart = (req, res) => {};
+const getCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = userData.cartData;
+    res.json({ success: true, cartData });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 export { addToCart, removeFromCart, getCart };
