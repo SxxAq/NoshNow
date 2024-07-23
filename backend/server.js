93aa2fb0406de47ffe8be0ddb274a0f39338c6bc
SxxAq <aqueel192@gmail.com>
2024-07-24 01:35:53 +0530

    stripe setup backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { connnectDB } from "./config/db.js";
 import foodRouter from "./routes/route.js";
 import userRouter from "./routes/userRoute.js";
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 import "dotenv/config";
 // app config
 const app = express();
@@ -21,6 +22,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
