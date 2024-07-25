efde4e273aebb85abfa6d91583a4bd9ca251180d
SxxAq <aqueel192@gmail.com>
2024-07-26 02:38:06 +0530

    status update backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -88,5 +88,15 @@ const listOrders = async (req, res) => {
     res.json({ success: false, message: "Error" });
   }
 };
+// api for order status
+const updateStatus=async(req,res)=>{
+try {
+  await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status})
+  res.json({success:true,message:"Status updated."})
+} catch (error) {
+  console.log(error);
+  res.json({success:false,message:"Error"})
+}
+}
 
-export { placeOrder, verifyOrder, userOrders, listOrders };
+export { placeOrder, verifyOrder, userOrders, listOrders,updateStatus };
