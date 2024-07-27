5eb8514acef368aec66b74504d01270f8735464f
SxxAq <aqueel192@gmail.com>
2024-07-27 14:55:54 +0530

    nav fixes

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -43,7 +43,7 @@ import food_29 from "./food_29.png";
 import food_30 from "./food_30.png";
 import food_31 from "./food_31.png";
 import food_32 from "./food_32.png";
-
+import logo_2 from "./logo-2.png";
 import add_icon_white from "./add_icon_white.png";
 import add_icon_green from "./add_icon_green.png";
 import remove_icon_red from "./remove_icon_red.png";
@@ -81,6 +81,7 @@ export const assets = {
   logout_icon,
   bag_icon,
   parcel_icon,
+  logo_2,
 };
 
 export const menu_list = [
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,77 +3,79 @@ import { assets } from "../../assets/assets";
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { StoreContext } from "../../context/StoreContext";
+
 const Navbar = ({ setShowLogin }) => {
   const navigate = useNavigate();
   const [menu, setMenu] = useState("home");
   const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+
   const onLogout = () => {
     localStorage.removeItem("token");
     setToken("");
     navigate("/");
   };
+
   return (
     <div className="navbar">
       <Link to="/">
-        <img src={assets.logo1} alt="logo" className="logo" />
+        <img src={assets.logo_2} alt="logo" className="logo" />
       </Link>
       <ul className="navbar-menu">
         <Link to="/">
           <li
-            onClick={() => {
-              setMenu("home");
-            }}
+            onClick={() => setMenu("home")}
             className={menu === "home" ? "active" : ""}
           >
-            home
+            Home
           </li>
         </Link>
-        <li
-          onClick={() => {
-            setMenu("menu");
-          }}
-          className={menu === "menu" ? "active" : ""}
-        >
-          menu
-        </li>
-        <li
-          onClick={() => {
-            setMenu("mobile-app");
-          }}
-          className={menu === "mobile-app" ? "active" : ""}
-        >
-          mobile-app
-        </li>
-        <li
-          onClick={() => {
-            setMenu("contact");
-          }}
-          className={menu === "contact" ? "active" : ""}
-        >
-          contact us
-        </li>
+        <a href="#explore-menu">
+          <li
+            onClick={() => setMenu("menu")}
+            className={menu === "menu" ? "active" : ""}
+          >
+            Menu
+          </li>
+        </a>
+        <a href="#app-download">
+          <li
+            onClick={() => setMenu("mobile-app")}
+            className={menu === "mobile-app" ? "active" : ""}
+          >
+            Mobile App
+          </li>
+        </a>
+        <a href="#footer">
+          {" "}
+          <li
+            onClick={() => setMenu("contact")}
+            className={menu === "contact" ? "active" : ""}
+          >
+            Contact Us
+          </li>
+        </a>
       </ul>
       <div className="navbar-right">
-        <img src={assets.search_icon} alt="" />
+        <img src={assets.search_icon} alt="Search" />
         <div className="navbar-search-icon">
           <Link to="/cart">
-            <img src={assets.basket_icon} alt="" />
+            <img src={assets.basket_icon} alt="Cart" />
           </Link>
-          {getTotalCartAmount() > 0 ? <div className="dot"></div> : <></>}
+          {getTotalCartAmount() > 0 && <div className="dot"></div>}
         </div>
         {!token ? (
           <button onClick={() => setShowLogin(true)}>Sign In</button>
         ) : (
           <div className="navbar-profile">
-            <img src={assets.profile_icon} alt="" />
+            <img src={assets.profile_icon} alt="Profile" />
             <ul className="nav-profile-dropdown">
-              <li>
-                <img src={assets.bag_icon} alt="" />
-                <p onClick={()=>navigate("/myorders")}>Orders</p>
+              <li onClick={() => navigate("/myorders")}>
+                <img src={assets.bag_icon} alt="Orders" />
+                <p>Orders</p>
               </li>
               <hr />
               <li onClick={onLogout}>
-                <img src={assets.logout_icon} alt="" />
+                <img src={assets.logout_icon} alt="Logout" />
                 <p>LogOut</p>
               </li>
             </ul>
